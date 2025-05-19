a0d2becc787fefa01e9f7778a8a64154af604742
julio Tuozzo <jtuozzo@hotmail.com>
2025-05-19 16:02:33 -0300

    Ya hay favicon. En el burger menú

---
+++ aplic/public/js/main.js
@@ -0,0 +1,10 @@
+/*  Nombre: main.js
+    Autor: Julio Tuozzo
+    Función: Javascript principal.
+    Fecha de creación: 19/05/2025.
+    Ultima modificación: 19/05/2025.
+*/
+
+function toggleMenu() {
+    $("#barraNavUl").toggleClass('mostrar');
+}
