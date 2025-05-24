84278fde1eb046950ebbdea5fb52689dfa70f2b9
julio Tuozzo <jtuozzo@hotmail.com>
2025-05-24 20:49:31 -0300

    Envía el mail de creación del usuario

--- aplic/public/usuario.php
+++ aplic/public/usuario.php
@@ -21,7 +21,7 @@ if(!isset($_POST['crear']))
       exit;
      }
 
-if($user->crearUsuario($nombres, $apellidos, $email, $clave ))
+if($user->crearUsuario($nombres, $apellidos, $email, $clave, $reingresa ))
      {// Se pudo crear el usuario
       $mensaje="<div class='creado'>Nuevo usuario creado. Verificá tu casilla la correo de <em>$email</em> para terminar el proceso.<br/> <br/>Si no llegó el mail, revisá la casilla de correo basura.</div>
       <script type='text/javascript'>ocultoForm();</script>";
