e8ebe50fb79f53886da8c483d4bd6d0880ec5428
julio Tuozzo <jtuozzo@hotmail.com>
2025-05-24 19:07:26 -0300

    Vista de la creación de usuarios

---
+++ aplic/public/js/usuario.js
@@ -0,0 +1,13 @@
+/*  Nombre: usuario.js
+    Autor: Julio Tuozzo
+    Función: Javascript de nuevo usuario.
+    Fecha de creación: 24/05/2025.
+    Ultima modificación: 24/05/2025.
+*/
+
+
+window.onload = function() {
+        var alt=$(window).height()-175;
+        $('#usuario').css({'height':alt+'px', 'overflow-y':'auto'});
+    }
+
