d19076415f625ef7a6b01fa26856bc4408893727
julio Tuozzo <jtuozzo@hotmail.com>
2025-05-27 12:42:13 -0300

    Métodos de validación de usuarios y listado de artículos

--- aplic/public/articulo_list.php
+++ aplic/public/articulo_list.php
@@ -32,9 +32,13 @@ $usuario = new User();
 
 if(isset($id))
      {$token = $id;
+      // Como no es el usuario dueño de los artículos, ve los que están solo en venta
+      $articulo->solo_en_venta = true;
      }
 else
      {$token = $_SESSION['DML_TOKEN'];
+      // Es el usuario, ve a todos los artículos
+      $articulo->solo_en_venta = false;
      }
 
 if(!$usuario->tokenValido($token))
@@ -43,7 +47,36 @@ if(!$usuario->tokenValido($token))
       exit;
      }    
 
+if(!isset($pagina))
+     {// No está paginando, es un query nuevo
+      $pagina = 1;
+      $sentido ="ASC";
+      $orden = "id";
+      $q_registros = $articulo->countArticulos($usuario->id);
+     }
+
+$desde = ($pagina - 1) * MAX_ARTICULOS;
+
+// Armo y ejecuto la consulta
+
+$query = $articulo->queryArticulos($usuario->id, $orden, $sentido);
+
 $result = Utils::selectLimit($query, $desde,__FILE__, __LINE__);
+
+
+if($sentido=="DESC")
+        {$_aux_var="arr_$orden";
+         $$_aux_var="&nbsp; &#9660;";
+         $_aux_var="sen_$orden";
+         $$_aux_var="ASC";
+        }
+else
+        {$_aux_var="arr_$orden";
+         $$_aux_var="&nbsp; &#9650;";
+         $_aux_var="sen_$orden";
+         $$_aux_var="DESC";
+        }
+
 
 require("articulo_list.inc");
 ?>
