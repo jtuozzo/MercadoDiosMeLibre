28d6fbfdbb64f6642d73fc2611e53e611e5db8f3
julio Tuozzo <jtuozzo@hotmail.com>
2025-05-28 16:15:02 -0300

    Método para validar los datos de los artículos

--- aplic/public/articuloGet.php
+++ aplic/public/articuloGet.php
@@ -30,11 +30,47 @@ require("Articulo.inc");
 
 $articulo = new Articulo;
 
-// Trae los datos del artículo
+if(isset($modificar))
+    {// Armo los array de fotos
+    $fotos_actuales = array();
+    $fotos_nuevas = array();
+    for($i=1; $i<=MAX_FOTOS; $i++)
+        {$aux="articulo_foto_id_{$i}";
+         if(isset($$aux))
+            {$fotos_actuales[]=$$aux;
+            }
+         elseif(isset($_FILES["foto_$i"]))
+            {$fotos_nuevas[]= $_FILES["foto_$i"]; 
+            }
+        }
 
-if (!$articulo->getArticulo($articulo_id))
-    {header("Location: index.php");
-     exit;
+        
+     if(!modifArticulo($articulo_id, $articulo->titulo, $articulo->descripcion, $articulo->moneda, $articulo->precio, $fotos_actuales, $fotos_nuevas))
+            {$mensaje="<script language='javascript'>
+                        Swal.fire({
+                        icon:'success',
+                        title:'Artículo modificado',
+                        confirmButtonColor: '#63676c',
+                        confirmButtonText: 'Continuar'
+                        })
+                       .then(function() {
+        	                window.parent.location = reload();
+                            window.close();
+                    });
+                     </script>";
+            }
+        else
+            {$mensaje=Utils::msgError();
+            }
+    }
+else
+    {// Trae los datos del artículo
+
+    if (!$articulo->getArticulo($articulo_id))
+        {header("Location: index.php");
+        exit;
+        }
+    $mensaje="";
     }
 
 // Defino el tipo de vista del artículo
@@ -42,7 +78,6 @@ if (!$articulo->getArticulo($articulo_id))
 if(isset($_SESSION['DML_USER_ID']) and $_SESSION['DML_USER_ID']==$articulo->user_id)
     {// El artículo es del usuario, es una vista de edición y lo puede modificar
      $articulo->vista="M";
-     $mensaje="";
      require("articulo_vista.inc");
     }
 else    
