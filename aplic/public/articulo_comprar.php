c3f1e4c6d285312073a26a09087064973bfd63fb
julio Tuozzo <jtuozzo@hotmail.com>
2025-05-31 18:13:04 -0300

    Clase compra

--- aplic/public/articulo_comprar.php
+++ aplic/public/articulo_comprar.php
@@ -13,7 +13,7 @@ session_start();
 require("Utils.inc");
 require("Articulo.inc");
 $articulo = new Articulo();
-$comprador = new Comprador();
+$compra = new Compra();
 
 // Inicializo GETS y POSTS y verifico el acceso al artículo
 
@@ -23,8 +23,8 @@ if(isset($_GET['id']) and isset($_GET['key']))
     }
 
 foreach($_POST as $clave => $valor)
-     {if(isset($comprador->$clave))
-          {$comprador->$clave=trim(htmlentities($valor,ENT_QUOTES,'UTF-8'));
+     {if(isset($compra->$clave))
+          {$compra->$clave=trim(htmlentities($valor,ENT_QUOTES,'UTF-8'));
           }
       else     
           {$$clave=trim(htmlentities($valor,ENT_QUOTES,'UTF-8'));
@@ -46,15 +46,15 @@ if(!isset($_POST['compro']))
      exit;
     }
 
-if($comprador->crearCompra())
-    {if($comprador->registrado)
+if($compra->crearCompra($articulo->articulo_id))
+    {if($compra->registrado)
         {
 
         }
      else
         {$mensaje="<div class='has_comprado'>
                     <h1>Excelente!!</h1>
-                    <h2>Te hemos enviado un correo a {$comprador->email}</h2>
+                    <h2>Te hemos enviado un correo a {$compra->email}</h2>
                     Verificá tu casilla la correopara terminar el proceso.<br/> <br/>Si no llegó el mail, revisá la casilla de correo basura.<br />
                     Si en el futuro querés saltear este paso de verificación, podés crear tu usuario aquí: <a href='usuario.php'>Crear Usuario</a>
           </div>";
