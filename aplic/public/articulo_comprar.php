ee2ef5c438cf6ba834733b6209875be210693fe1
julio Tuozzo <jtuozzo@hotmail.com>
2025-05-31 15:13:02 -0300

    Controlador de la compra

--- aplic/public/articulo_comprar.php
+++ aplic/public/articulo_comprar.php
@@ -13,6 +13,7 @@ session_start();
 require("Utils.inc");
 require("Articulo.inc");
 $articulo = new Articulo();
+$comprador = new Comprador();
 
 // Inicializo GETS y POSTS y verifico el acceso al artículo
 
@@ -40,4 +41,29 @@ if(!isset($_POST['comprar']))
     {require ("articulo_comprar_vista.inc");
      exit;
     }
+
+if($comprador->crearCompra())
+    {if($comprador->registrado)
+        {
+
+        }
+     else
+        {$mensaje="<div class='has_comprado'>
+                    <h1>Excelente!!</h1>
+                    <h2>Te hemos enviado un correo a {$comprador->email}</h2>
+                    Verificá tu casilla la correopara terminar el proceso.<br/> <br/>Si no llegó el mail, revisá la casilla de correo basura.<br />
+                    Si en el futuro querés saltear este paso de verificación, podés crear tu usuario aquí: <a href='usuario.php'>Crear Usuario</a>
+          </div>";
+
+        }
+
+     $mensaje.="<script type='text/javascript'>ocultoForm();</script>
+         </div>";
+    }
+else
+    {// No se pudo procesar la compra
+     $mensaje=Utils::msgError();
+    }
+
+require ("articulo_comprar_vista.inc");
 ?>
