5a05b6b0a5a7843e608dbf04083e651e7a5f411a
julio Tuozzo <jtuozzo@hotmail.com>
2025-06-03 20:16:46 -0300

    Arreglé el tamaño de Nada para Vender

--- aplic/public/js/ventas_list.js
+++ aplic/public/js/ventas_list.js
@@ -2,10 +2,14 @@
     Autor: Julio Tuozzo
     Función: Javascript del listado de artículos vendidos.
     Fecha de creación: 02/06/2025.
-    Ultima modificación: 02/06/2025.
+    Ultima modificación: 03/06/2025.
 */
 
 
 window.onload = function () {
     setAlto('list_ventas');
+}
+
+function ocultarEncabezado() {
+    $('.encabezado').hide();
 }
