c56f984c617d8128bed2de69c7e65992689035c5
julio Tuozzo <jtuozzo@hotmail.com>
2025-06-03 19:25:41 -0300

    Ver / no ver password

--- aplic/public/js/cambiar_clave.js
+++ aplic/public/js/cambiar_clave.js
@@ -2,10 +2,22 @@
     Autor: Julio Tuozzo
     Función: Javascript del login de usuario.
     Fecha de creación: 24/05/2025.
-    Ultima modificación: 01/06/2025.
+    Ultima modificación: 03/06/2025.
 */
 
 
 window.onload = function() {
         setAlto('cambiar_clave');
+    }
+
+function changeNuevaClave() {
+    if($('#ver_nueva_clave_img').attr('src')=='./images/ver.png')
+        {$('#ver_nueva_clave_img').attr('src', './images/no_ver.png'); ~
+         $('#nueva_clave').attr('type', 'text'); 
+         
+        }
+    else
+        {$('#ver_nueva_clave_img').attr('src', './images/ver.png'); 
+         $('#nueva_clave').attr('type', 'password'); 
+        }
     }
