1a74e4993750350d11bd2f52a8b58346f0551a7d
julio Tuozzo <jtuozzo@hotmail.com>
2025-06-13 11:43:28 -0300

    Dejo la creación de usuario como estaba y voy a arpar un nuevo controlador para la modificación del usuario

--- aplic/public/usuario.php
+++ aplic/public/usuario.php
@@ -4,7 +4,7 @@
     Autor: Julio Tuozzo.
     Función: Controlador del usuario.
     Fecha de creación: 24/05/2025.
-    Ultima modificación: 08/06/2025.
+    Ultima modificación: 13/06/2025.
 */
 
 session_start();
@@ -31,9 +31,7 @@ if(!isset($_POST['crear']))
       exit;
      }
 
-if(!isset($_SESSION['DML_USER_ID']))
-     {// No hay usuario sesionado, es un nuevo usuario
-     if($user->crearUsuario($clave, $reingresa ))
+if($user->crearUsuario($clave, $reingresa ))
           {// Se pudo crear el usuario
           $mensaje="<div class='creado'>Nuevo usuario creado.<br/> Verificá tu casilla la correo de <em>$email</em> para terminar el proceso.<br/> <br/>Si no llegó el mail, revisá la casilla de correo basura.</div>
           <script type='text/javascript'>ocultoID('form');</script>";
@@ -48,26 +46,6 @@ if(!isset($_SESSION['DML_USER_ID']))
                }
           
           }
-
-     }
-elseif(!isset($_SESSION['DML_USER_PASS']))
-     {// Está sesionado, vuelvo a validar la password
-
-      $mensaje="";
-      
-
-     }
-else 
-     {// Hay usuario sesionado y validado, proceso la modificación modificación
-      if($user->modifUsuario($clave, $reingresa))
-          {$mensaje = "TODO OK";
-           unset($_SESSION['DML_USER_PASS']);
-          }
-      else
-          {$mensaje=Utils::msgError();
-          }
-
-     }
 
 require("usuario_vista.inc");
 ?>
