d13c0351839318e60944b15063ed51b0499bfccc
julio Tuozzo <jtuozzo@hotmail.com>
2025-06-13 12:37:36 -0300

    Controlador de cambio de datos del usuario

--- aplic/public/usuario_mod.php
+++ aplic/public/usuario_mod.php
@@ -1,9 +1,9 @@
 <?php
 /*
-    Nombre: usuario.php
+    Nombre: usuario_mod.php
     Autor: Julio Tuozzo.
-    Función: Controlador del usuario.
-    Fecha de creación: 24/05/2025.
+    Función: Controlador de modificación de datos de usuario.
+    Fecha de creación: 13/06/2025.
     Ultima modificación: 13/06/2025.
 */
 
@@ -14,8 +14,14 @@ require("User.inc");
 
 $user = new User();
 
-// Defino la vista del usuario como Alta
-$user->vista="A";
+if(!$user->getUser($_SESSION['DML_EMAIL'],$_SESSION['DML_CLAVE']))
+     {// No está bien sesionado, se va
+      header("Location: login.php");
+      exit;
+     }
+
+// Defino la vista del usuario como Modificación
+$user->vista="M";
 
 foreach($_POST as $key => $valor)
      {if(isset($user->$key))
@@ -27,27 +33,21 @@ foreach($_POST as $key => $valor)
      }
 
 
-if(!isset($_POST['crear']))
+if(!isset($_POST['guardar']))
      {$mensaje="";
       require("usuario_vista.inc");
       exit;
      }
 
-if($user->crearUsuario($clave, $reingresa ))
-          {// Se pudo crear el usuario
+if($user->modifUsuario($clave))
+          {// Se pudo modificar el usuario
           $mensaje="<div class='creado'>Nuevo usuario creado.<br/> <br/>Verificá tu casilla de correo <em>{$user->email}</em> para terminar el proceso.<br/> <br/>Si no llegó el mail, revisá la casilla de correo basura.</div>
           <script type='text/javascript'>ocultoID('form');</script>";
           }
      else
-          {// No se pudo crear el usuario
-          if(strlen($user->send_mail_err)>0)
-               {$mensaje=Utils::msgError($user->send_mail_err, "Intente más tarde.");
-               }
-          else 
-               {$mensaje=Utils::msgError();
-               }
-          
+          {// No se pudo modificar el usuario
+           $mensaje=Utils::msgError();
           }
-
+          
 require("usuario_vista.inc");
 ?>
