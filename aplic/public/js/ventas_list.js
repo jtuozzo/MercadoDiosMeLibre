091ed56bf100e07ed834b05f586aa316ffbd41bc
julio Tuozzo <jtuozzo@hotmail.com>
2026-01-25 19:04:29 -0300

    JS Block UI

--- aplic/public/js/ventas_list.js
+++ aplic/public/js/ventas_list.js
@@ -2,7 +2,7 @@
     Autor: Julio Tuozzo
     Función: Javascript del listado de artículos vendidos.
     Fecha de creación: 02/06/2025.
-    Ultima modificación: 03/06/2025.
+    Ultima modificación: 25/01/2026.
 */
 
 
@@ -13,3 +13,56 @@ window.onload = function () {
 function ocultarEncabezado() {
     $('.encabezado').hide();
 }
+
+function compraDelete(articulo_compra_id, nombres, apellidos) {
+    Swal.fire({
+        text: 'Elimina la compra de ' + nombres + ' ' + apellidos + '??',
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#1b998b',
+        cancelButtonColor: '#d7263d',
+        cancelButtonText: 'No',
+        confirmButtonText: 'Si',
+        focusCancel: true
+    })
+        .then((result) => {
+            if (result.isConfirmed) {
+
+                $.get('compra_delete.php',
+                   {articulo_compra_id: articulo_compra_id
+                    },
+               function(data, status)
+                        {
+                         if(status!='success')
+                               { alert('Estado: ' + status);
+                               }
+                         else if(data.length>0)
+                                    {window.alert(data)
+                                    }
+                               else
+                                    {window.location.reload();
+                                    }
+                         }
+
+                   );
+            }
+
+        }
+        )
+}
+
+$(document).ajaxStart(function () {
+    $.blockUI({
+        message: "<img src='./images/loading_136_136.gif'  />",
+        css: {
+            padding: '20px',
+            border: 'none',
+            width: '160px',
+            left: ($(window).width() - 160) / 2 + 'px',
+        }
+    })
+})
+
+$(document).ajaxStop(
+    $.unblockUI
+)
